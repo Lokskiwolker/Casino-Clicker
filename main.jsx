b028d2985eab7022440cdd7eba1a829178b05701
Lokskiwolker <51489537+Lokskiwolker@users.noreply.github.com>
2026-09-02 22:18:31 +0100

    Remove main
    Simplify estate

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './lucky-tap_3.jsx' // Links to your file
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
